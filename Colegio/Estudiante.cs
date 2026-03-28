c7d65bf2417eee8025332a9049857a9c7dc8d7ca
Esteban <juanestebanhernandezflorez097@gmail.com>
2026-03-28 18:56:45 -0500

    Algunos cambios a la clase Estudiante

--- Colegio/Estudiante.cs
+++ Colegio/Estudiante.cs
@@ -6,4 +6,23 @@ public class Estudiante
     public string Direccion { get; get; }
     public string celular { get; set; }
     public string Correo { get; set; }
-}
+
+ // Constructor con todos los atributos//
+        public Estudiante(int codigo, string nombre, string apellido, string direccion, string celular, string email)
+        {
+            this.Codigo = codigo;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Direccion = direccion;
+            this.Celular = celular;
+            this.Email = Correo;
+        }
+
+        // Método ToString
+        public override string ToString()
+        {
+            return $"Estudiante: {this.Nombre} {this.Apellido}, Código: {this.Codigo}, Dirección: {this.Direccion}, Celular: {this.Celular}, Email: {this.Email}";
+        }
+    }
+
+
