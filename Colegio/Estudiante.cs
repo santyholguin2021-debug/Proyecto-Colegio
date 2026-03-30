790c41f81ab16f1f7b3e3578cade3197bdfe0f0d
Esteban <juanestebanhernandezflorez097@gmail.com>
2026-03-29 20:40:23 -0500

    Clase Completa Estudiante

--- Colegio/Estudiante.cs
+++ Colegio/Estudiante.cs
@@ -5,23 +5,23 @@ public class Estudiante
     public string Apellido { get; set; }
     public string Direccion { get; get; }
     public string celular { get; set; }
-    public string Correo { get; set; }
+    public string email { get; set; }
 
  // Constructor con todos los atributos//
-        public Estudiante(int codigo, string nombre, string apellido, string direccion, string celular, string email)
+        public Estudiante(int Id, string nombre, string apellido, string direccion, string celular, string email)
         {
-            this.Codigo = codigo;
+            this.Codigo = Id;
             this.Nombre = nombre;
             this.Apellido = apellido;
             this.Direccion = direccion;
             this.Celular = celular;
-            this.Email = Correo;
+            this.email = email;
         }
 
         // Método ToString
         public override string ToString()
         {
-            return $"Estudiante: {this.Nombre} {this.Apellido}, Código: {this.Codigo}, Dirección: {this.Direccion}, Celular: {this.Celular}, Email: {this.Email}";
+            return $"Estudiante: {this.Nombre} {this.Apellido}, id: {this.Id}, Dirección: {this.Direccion}, Celular: {this.Celular}, email: {this.email}";
         }
     }
 
